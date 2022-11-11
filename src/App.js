8890e53869dc9ba351fc3dfc1c2866f3f0ef5dd8
rafacancian <cancian.rafa@gmail.com>
2022-11-11 07:22:54 -0300

    create banner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
+          <Banner />
     </div>
   );
 }
---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" alt="The Banner"/>
+        </header>
+    )
+}
+
+export default Banner
